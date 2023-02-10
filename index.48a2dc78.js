7e45fb8f37913a55c1a92b388fa6194d7473994b
jimmy  (°O°) <117820706+jimmyomont@users.noreply.github.com>
2023-02-10 08:45:00 +0100

    Updates

--- index.48a2dc78.js
+++
@@ -1,2 +0,0 @@
-function setClass(s){document.body.className=s}window.addEventListener("scroll",(()=>{const s=window.scrollY;setClass(s<500?"scrollhaut":s<1e3?"scrollmiddle":"red")}));
-//# sourceMappingURL=index.48a2dc78.js.map
