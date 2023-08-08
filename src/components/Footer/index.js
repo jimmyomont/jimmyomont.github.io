3bc0fa5f27b935b18e4395cda2f4ae0fd5a45fca
jimmy  (°O°) <117820706+jimmyomont@users.noreply.github.com>
2023-08-08 17:51:08 +0200

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,10 +4,10 @@ function Footer() {
     return (
         <>
             <p className="footer">
-                <p>Portfolio de Jimmy Omont</p>
+                <p className="footer-content" >Portfolio de Jimmy Omont</p>
                 <a className="lien" href='mailto:jimmy-omont@hotmail.fr'>jimmy.omont@hotmail.fr</a>
-                <p>06.66.25.21.41</p>
-                <p><a className="lien" href="https://www.linkedin.com/in/jimmy-omont-141471166/">Profil LinkeIn</a></p>
+                <p className="footer-content">06.66.25.21.41</p>
+                <p className="footer-content"><a className="lien" href="https://www.linkedin.com/in/jimmy-omont-141471166/">Profil LinkeIn</a></p>
             </p>
         </>
     )
